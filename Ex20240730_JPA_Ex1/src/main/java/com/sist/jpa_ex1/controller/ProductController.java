9419e7a862f5216bc40685e68bb169eefc45b79d
wjsxorjs <wjsxorjs@naver.com>
2024-07-30 17:36:25 +0900

    20240730 Spring 1.0

--- Ex20240730_JPA_Ex1/src/main/java/com/sist/jpa_ex1/controller/ProductController.java
+++ Ex20240730_JPA_Ex1/src/main/java/com/sist/jpa_ex1/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.sist.jpa_ex1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sist.jpa_ex1.repository.ProductRepository;
 import com.sist.jpa_ex1.store.ProductJPO;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ProductController {
@@ -24,4 +28,24 @@ public class ProductController {
 
         return "TEST";
     }
+
+    @GetMapping("list")
+    public String getList() {
+        List<ProductJPO> list = productRepository.findAll();
+        StringBuffer sb = new StringBuffer();
+        for(ProductJPO pvo: list){
+            sb.append(pvo.getPNum());
+            sb.append(" || ");
+            sb.append(pvo.getPName());
+            sb.append(" || ");
+            sb.append(pvo.getC1vo().getCName());
+            sb.append(" || ");
+            sb.append(pvo.getC1vo().getDesc());
+            sb.append(" <br/> ");
+        }
+
+
+        return sb.toString();
+    }
+    
 }
