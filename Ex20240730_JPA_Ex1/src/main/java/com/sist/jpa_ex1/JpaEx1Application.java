eab77d7cf477db2fbf071310e2d386ab4bebecd7
wjsxorjs <wjsxorjs@naver.com>
2024-07-30 15:31:01 +0900

    20240730 Spring 0.75

---
+++ Ex20240730_JPA_Ex1/src/main/java/com/sist/jpa_ex1/JpaEx1Application.java
@@ -0,0 +1,13 @@
+package com.sist.jpa_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEx1Application.class, args);
+	}
+
+}
